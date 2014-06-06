f99b34cd0f231e961d27a9fb1914fd64f9c7d084
Travis Miller <travisbmiller@outlook.com>
2014-06-05 19:16:17 -0700

    Array_Definition Completed

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,16 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(num1,num2,num3,num4){
+  var numbers = [];
+  numbers.push(num1);
+  numbers.push(num2);
+  numbers.push(num3);
+  numbers.push(num4);
+  return numbers;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array){
+  var newArray = []
+  newArray.push(array.shift());
+  newArray.push(array.pop())
+  return newArray;
+
+};
