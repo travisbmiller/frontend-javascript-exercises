1a335ce78be8340a84fddf53fa4428afad7447f4
Travis Miller <travisbmiller@outlook.com>
2014-06-05 21:09:34 -0700

    Array Functions Completed

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,21 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array){
+  array.reverse();
+  array.unshift(1);
+  return array;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array){
+  var newString = "";
+  for(var i = 0; i < array.length; i++){
+    if(newString.length === 0){
+      newString = newString + array[i];
+    } else {
+      newString = newString + "+" + array[i];
+    }
+  }
+  return newString;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array){
+  return array.length + 1;
+};
