67f919d0d1e5f7614b2038d99d6fff703ffeb1cd
Travis Miller <travisbmiller@outlook.com>
2014-06-08 15:38:42 -0700

    00 loops array completed

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+    sum = sum + array[i];
+  }
+  return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(string){
+  var newArray = string.toLowerCase().split(",");
+  return newArray;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array){
+  var ret = [];
+  for(var i = 0; i < array.length; i++){
+    ret.push(i + " is " + array[i]);
+  }
+  return ret;
+};
