6a13a7d7ec0cc2bc06e10ca1e872b42497d0a67d
Travis Miller <travisbmiller@outlook.com>
2014-06-08 15:49:39 -0700

    01 loop over objects completed

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object){
+  var ret = [];
+  for(val in object){
+    ret.push(val)
+  }
+  return ret;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object){
+  var ret = [];
+  for(val in object){
+    ret.push(object[val]);
+  }
+  return ret;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object){
+  var ret = [];
+  for(val in object){
+    ret.push(val + " is " + object[val]);
+  }
+  return ret;
+};
