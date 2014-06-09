7f5ce75280dbf90681764b0316f868d3cf92fe46
Travis Miller <travisbmiller@outlook.com>
2014-06-09 02:46:01 -0700

    03 while loops completed

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,20 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn,actionFn){
+  while (conditionalFn() === true ) {
+    actionFn();
+  }
+  
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr){
+  var count = arr.length,
+      sum = 0,
+      ret;
+
+  while(count > 0){
+    sum = arr.shift() + sum;
+    count--;
+  }
+  return sum;
+
+};
 
