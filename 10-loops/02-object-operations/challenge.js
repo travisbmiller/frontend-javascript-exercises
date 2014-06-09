427f3c91844d27edb7b3c59d1f36071c0b4d4ee3
Travis Miller <travisbmiller@outlook.com>
2014-06-09 01:46:32 -0700

    object 02 compeleted

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,41 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object){
+ var result = {};
+ 
+ result = JSON.parse(JSON.stringify(object));
+ 
+ return result;
 
-module.exports.extend = undefined;
+};
 
-module.exports.hasElems = undefined;
+module.exports.extend = function(dest,src){
+  var key;
+
+  for(key in src){
+    dest[key] = src[key]; 
+  }
+
+  return dest;
+
+};
+
+module.exports.hasElems = function(obj,arr){
+  var newArr = [],
+      ret = true;
+  
+  if(arr.length > 1){
+    
+    for(i=0; i < arr.length; i++){
+      
+      if(obj[arr[i]] === undefined){
+       
+       ret = false;
+      
+      }
+    
+    } 
+  
+  };
+
+  return ret;
+
+};
